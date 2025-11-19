033b5f32ab36140485062fd4c5e25b9b1ae8f75d
Fauzan <macbook@Fauzann.local>
2025-11-19 15:01:18 +0700

    New 2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,14 +4,14 @@ module.exports = {
       name: "cctv-backend",
       script: "app.py",
       interpreter: "C:/Users/Administrator/miniconda3/envs/cctv/python.exe",
-      cwd: "C:/Users/Administrator/CCTV-Detection-Factory/backend",
+      cwd: "C:/Users/Administrator/Projects/CCTV-Detection-Factory/backend",
       watch: false
     },
     {
       name: "cctv-scheduler",
       script: "scheduler.py",
       interpreter: "C:/Users/Administrator/miniconda3/envs/cctv/python.exe",
-      cwd: "C:/Users/Administrator/CCTV-Detection-Factory/backend",
+      cwd: "C:/Users/Administrator/Projects/CCTV-Detection-Factory/backend",
       watch: false
     }
   ]
